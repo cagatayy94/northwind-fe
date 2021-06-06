54ac50e29dbad11fcbf25175271c15b0203b03aa
cagatayy94 <mustafa.cagatay.yilmaz@gmail.com>
2021-06-07 02:08:49 +0300

    Added product fetcher

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
 import React from "react";
+import Dashboard from './layouts/Dashboard';
+import Navi from './layouts/Navi'
+import 'semantic-ui-css/semantic.min.css'
+import { Container } from 'semantic-ui-react';
 
 function App() {
   return (
     <div className="App">
-
+      <Navi/>
+      <Container className="main">
+        <Dashboard/>
+      </Container>
     </div>
   );
 }
